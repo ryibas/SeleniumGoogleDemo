a1a38a052e2fb9aca184ebffee71f98c459d5f8d
Baseman <Ryan.J.Baseman@ampf.com>
2018-03-08 12:08:26 -0600

    Incremental work

--- Selenium_Demo_4.js
+++ Selenium_Demo_4.js
@@ -2,12 +2,25 @@ require('chromedriver');
 const {Builder, By, Key, until} = require('selenium-webdriver');
 
 async function checkGoogle() {
+
     let driver = new Builder().forBrowser('chrome').build();
+
     await driver.get('http://google.com/ncr');
+    await driver.sleep(5000);
     await driver.findElement(By.name('q')).sendKeys('What is 2 + 2?', Key.RETURN);
+    await driver.sleep(5000);
     await driver.wait(until.elementLocated(By.id('cwos')), 10000);
+
+    await driver.findElement(By.id('cwos')).getText().then(function (text) {
+        if (text === '4') {
+            console.log('Google can add today.');
+        } else {
+            console.log('Uh oh - run for the hills.');
+        }
+    });
+
     await driver.quit();
 }
 
 checkGoogle()
-    .then(_ => console.log('SUCCESS'), e => console.error('FAILURE: ' + e));
+    .then(_ => console.log('Test run complete.'), e => console.error('FAILURE: ' + e));

d97f3e188fcbfbad4a60596efc5cf4982724c53f
Baseman <Ryan.J.Baseman@ampf.com>
2018-03-28 07:18:49 -0500

    Demo Prep

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # SeleniumGoogleDemo
+
+
+
+let driver = new Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().setMobileEmulation({deviceName: 'Nexus 6P'}).addArguments('test-type')).build();
+
+
+Docs - http://seleniumhq.github.io/selenium/docs/api/javascript/index.html
--- Selenium_Demo.js
+++ Selenium_Demo.js
@@ -1,11 +1,15 @@
+var chrome = require('selenium-webdriver/chrome');
 require('chromedriver');
+
 const {Builder, By, Key, until} = require('selenium-webdriver');
 
+////////////////////////////////////////////////////////////////////////////////////////////////
+// Function to test whether or not searching Google for the answer for 2 + 2 does indeed = 4.
 async function checkGoogle() {
 
     let driver = new Builder().forBrowser('chrome').build();
-
-    await driver.get('http://google.com/ncr');
+    
+    await driver.get('http://google.com');
     await driver.sleep(5000);
     await driver.findElement(By.name('q')).sendKeys('What is 2 + 2?', Key.RETURN);
     await driver.sleep(5000);
